4ce3abd49fc08e97261c8e4bf8be0fc3368fb6ab
Rizki Ramadhan <rizukirimu@gmail.com>
2021-04-15 10:29:00 +0700

    Fix Transactions history

--- app/controllers/transactionController.js
+++ app/controllers/transactionController.js
@@ -1,98 +1,121 @@
 // Import Modules
-const moment = require('moment')
+const moment = require("moment");
 
 // Import Model
-const Transactions = require('../models/Transactions')
-const Showtimes = require('../models/Showtimes')
-const SoldSeats = require('../models/SoldSeats')
+const Transactions = require("../models/Transactions");
+const Showtimes = require("../models/Showtimes");
+const SoldSeats = require("../models/SoldSeats");
 
 // Import Helpers
-const response = require('../helpers/Response')
+const response = require("../helpers/Response");
 
 exports.buyTicket = async (req, res) => {
   try {
-    const result = await Transactions.create(req.data.id, req.body)
+    const result = await Transactions.create(req.data.id, req.body);
 
     if (!result) {
-      return response(res, 400, false, 'Failed to buy a ticket')
+      return response(res, 400, false, "Failed to buy a ticket");
     } else {
       const soldSeatsList = await SoldSeats.getSoldSeatByCondition({
         movieId: req.body.movieId,
         cinemaId: req.body.cinemaId,
         id: req.body.showTimeId,
-        timeId: req.body.timeId
-      })
-      const isSoldSeatExists = []
-      soldSeatsList.forEach(item => {
+        timeId: req.body.timeId,
+      });
+      const isSoldSeatExists = [];
+      soldSeatsList.forEach((item) => {
         if (req.body.seats.includes(item.seatCode.trim())) {
-          isSoldSeatExists.push(true)
+          isSoldSeatExists.push(true);
         } else {
-          isSoldSeatExists.push(false)
+          isSoldSeatExists.push(false);
         }
-      })
+      });
       if (isSoldSeatExists.indexOf(true) !== -1) {
-        const results = await Transactions.delete(result.id)
+        const results = await Transactions.delete(result.id);
         if (!results) {
-          return response(res, 400, false, "Can't remove transaction")
+          return response(res, 400, false, "Can't remove transaction");
         }
-        return response(res, 400, false, 'Sorry the selected seat has been used')
+        return response(
+          res,
+          400,
+          false,
+          "Sorry the selected seat has been used"
+        );
       }
       try {
-        const result = await SoldSeats.create(req.body.showTimeId, req.body.seats.split(','))
+        const result = await SoldSeats.create(
+          req.body.showTimeId,
+          req.body.seats.split(",")
+        );
         if (!result) {
-          return response(res, 400, false, 'Failed to add sold seat')
+          return response(res, 400, false, "Failed to add sold seat");
         } else {
-          return response(res, 200, true, 'Thank you for the order :)')
+          return response(res, 200, true, "Thank you for the order :)");
         }
       } catch (err) {
-        console.log(err)
-        return response(res, 500, false, 'Server Error')
+        console.log(err);
+        return response(res, 500, false, "Server Error");
       }
     }
   } catch (error) {
-    console.log(error)
-    return response(res, 500, false, 'Server Error')
+    console.log(error);
+    return response(res, 500, false, "Server Error");
   }
-}
+};
 
 exports.getUserOrderHistory = async (req, res) => {
   try {
-    const results = await Transactions.getOrderHistory(req.data.id)
+    const results = await Transactions.getOrderHistory(req.data.id);
 
     if (results.length < 1) {
-      return response(res, 400, false, 'Empty order history', results)
+      return response(res, 400, false, "Empty order history", results);
     } else {
       const orderHistory = results.map((item, index, array) => {
         return {
           ...array[index],
-          showTimeDate: moment(item.showTimeDate).format('DD MMMM YYYY'),
-
-        }
-      })
-      return response(res, 200, true, 'Get Order History Successfully', orderHistory)
+          showTimeDate: moment(item.showTimeDate).format("DD MMMM YYYY"),
+        };
+      });
+      return response(
+        res,
+        200,
+        true,
+        "Get Order History Successfully",
+        orderHistory
+      );
     }
   } catch (err) {
-    console.log(err)
-    return response(res, 500, false, 'Server Error')
+    console.log(err);
+    return response(res, 500, false, "Server Error");
   }
-}
+};
 
 exports.getUserOrderHistoryDetail = async (req, res) => {
   try {
-    const results = await Transactions.getOrderHistoryDetail(req.params.id, req.data.id)
+    const results = await Transactions.getOrderHistoryDetail(
+      req.params.id,
+      req.data.id
+    );
 
     if (results.length < 1) {
-      return response(res, 400, false, 'Empty order history', results)
+      return response(res, 400, false, "Empty order history", results);
     } else {
       const orderHistory = {
         ...results[0],
-        showTimeDate: moment(results[0].showTimeDate).format('DD MMMM YYYY'),
-        cinemaPoster: process.env.APP_URL + '/public/cinemas/' + results[0].cinemaPoster
-      }
-      return response(res, 200, true, 'Get Order History Successfully', orderHistory)
+        showTimeDate: moment(results[0].showTimeDate).format("DD MMMM YYYY"),
+        cinemaPoster:
+          process.env.APP_URL + "/public/cinemas/" + results[0].cinemaPoster,
+      };
+      return response(
+        res,
+        200,
+        true,
+        "Get Order History Successfully",
+        orderHistory
+      );
     }
   } catch (err) {
-    console.log(err)
-    return response(res, 500, false, 'Server Error')
+    console.log(err);
+    return response(res, 500, false, "Server Error");
   }
-}
+};
--- app/models/Transactions.js
+++ app/models/Transactions.js
@@ -1,12 +1,12 @@
-const Database = require('../config/Database')
+const Database = require("../config/Database");
 
 class Transactions extends Database {
-  constructor (table) {
-    super(table)
-    this.table = table
+  constructor(table) {
+    super(table);
+    this.table = table;
   }
 
-  create (userId, body) {
+  create(userId, body) {
     const data = {
       userId,
       ticketDate: body.ticketDate,
@@ -18,79 +18,79 @@ class Transactions extends Database {
       ticketCount: body.ticketCount,
       seats: body.seats,
       totalPayment: body.totalPayment,
-      paymentMethod: body.paymentMethod
-    }
+      paymentMethod: body.paymentMethod,
+    };
 
-    const sql = `INSERT INTO ${this.table} SET ?`
+    const sql = `INSERT INTO ${this.table} SET ?`;
     return new Promise((resolve, reject) => {
       const query = this.db.query(sql, data, (err, result) => {
         if (err) {
-          return reject(err)
+          return reject(err);
         } else if (result.affectedRow < 1) {
-          return resolve(false)
+          return resolve(false);
         } else {
           return resolve({
-            id: result.insertId
-          })
+            id: result.insertId,
+          });
         }
-      })
-      console.log(query.sql)
-    })
+      });
+      console.log(query.sql);
+    });
   }
 
-  getShowTimeId (ticketDate) {
+  getShowTimeId(ticketDate) {
     return new Promise((resolve, reject) => {
-      const sql = 'SELECT id FROM showtimes WHERE ticketDate = ?'
+      const sql = "SELECT id FROM showtimes WHERE ticketDate = ?";
       this.db.query(sql, ticketDate, (err, result) => {
         if (err) {
-          return reject(err)
+          return reject(err);
         } else {
-          return resolve(result)
+          return resolve(result);
         }
-      })
-    })
+      });
+    });
   }
 
-  getOrderHistory (userId) {
-    const sql = `SELECT id, ticketDate, ticketTime, cinemaPoster, cinemaName FROM ${this.table} WHERE userId = ?`
+  getOrderHistory(userId) {
+    const sql = `SELECT id, ticketDate, ticketTime, movieTitle cinemaPoster, cinemaName FROM ${this.table} WHERE userId = ?`;
     return new Promise((resolve, reject) => {
       this.db.query(sql, userId, (err, result) => {
         if (err) {
-          return reject(err)
+          return reject(err);
         } else {
-          return resolve(result)
+          return resolve(result);
         }
-      })
-    })
+      });
+    });
   }
 
-  getOrderHistoryDetail (id, userId) {
-    const sql = `SELECT * FROM ${this.table} WHERE userId = ? AND id = ?`
+  getOrderHistoryDetail(id, userId) {
+    const sql = `SELECT * FROM ${this.table} WHERE userId = ? AND id = ?`;
     return new Promise((resolve, reject) => {
       this.db.query(sql, [userId, id], (err, result) => {
         if (err) {
-          return reject(err)
+          return reject(err);
         } else {
-          return resolve(result)
+          return resolve(result);
         }
-      })
-    })
+      });
+    });
   }
 
-  delete (id) {
-    const sql = `DELETE FROM ${this.table} WHERE id = ?`
+  delete(id) {
+    const sql = `DELETE FROM ${this.table} WHERE id = ?`;
     return new Promise((resolve, reject) => {
       this.db.query(sql, [id], (err, result) => {
         if (err) {
-          return reject(err)
+          return reject(err);
         } else if (result.affectedRow < 1) {
-          return resolve(false)
+          return resolve(false);
         } else {
-          return resolve(true)
+          return resolve(true);
         }
-      })
-    })
+      });
+    });
   }
 }
 
-module.exports = new Transactions('transactions')
+module.exports = new Transactions("transactions");
